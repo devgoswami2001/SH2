38d3057d33c668b779cb4bda918af041fecf2239
Dev goswami <131504079+devgoswami2001@users.noreply.github.com>
2026-01-15 12:39:45 +0530

    Comment out SwipeToHireSection in page.tsx
    Comment out SwipeToHireSection for future reference.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
       <AiDemoSection />
       <HowItWorksSection />
       <AiBenefitsSection />
-      <SwipeToHireSection />
+      {/* <SwipeToHireSection /> */}
       <PoweredByHyreSenseAiSection />
       <TestimonialsSection />
       <CtaSection />
